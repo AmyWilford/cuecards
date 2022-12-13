7d214dbbf4dc3311868679d54297248c193d371d
Amy <wilford.amy@gmail.com>
2022-12-13 13:11:20 -0500

    added navigation to home and study pages to link between the two

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function Question() {
+
+export default function Home() {
+
+    let navigate = useNavigate();
+  const routeChange = () => {
+    let path = `/study`;
+    navigate(path);
+  };
+
     return (
         <div>
-            <h1> Hello</h1>
+            <button onClick = {routeChange}>Start Studying</button>
         </div>
     )
 }
