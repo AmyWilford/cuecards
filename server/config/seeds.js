6de33aac4a0d346bf1de46c16de4ba3eb2380444
Amy <wilford.amy@gmail.com>
2023-01-07 16:37:32 -0500

    added seed.js command to config file

---
+++ server/config/seeds.js
@@ -0,0 +1,49 @@
+const db = require('./connection');
+const { Question } = require('../models');
+
+db.once('open', async () => {
+  await Question.deleteMany();
+
+  const questions = await Question.insertMany([
+    {
+        "question": "What is the use of window object?",
+        "answer": "The window object represents an open window in a browser. BOM (the browser object model), allows Javascript to talk to the browser.  The DOM  (document object model is a property of the window object). All global javascript objects, functions, and variables automatically become members of the window object. Global variables are properties of the window object. Global functions are methods of the window object. There are some built in methods we can use to access or control the window - including .open(), .close(), moveTo(), resizeTo(), innerWidth(), innerHeight() > the later 2 are used to determine the size of the browser and will return in px."
+      },
+      {
+        "question": "What are the different data types present in JavaScript",
+        "answer":
+          "Primitive Value Types (BBUNNSS): Boolean, BigInt, Number, NaN, String, Symbol | Non-primitive Value Types: Objects, Arrays, Classes"
+      },
+      {
+        "question":
+          "What is the difference between primitive and non-primitive data types?",
+        "answer": "Primitive data types are predefined  and will always a have a value, while Non-primitive data types can be null. Primitive type starts with a lower caseletter, while non–primitive data types can start with an uppercase letter. The size of a primitive type depends on the data type - while non-primitive types all have the same size."
+      },
+      {
+        "question": "What is the difference between undefined value and null value?",
+        "answer": "Null refers to a variable whose value has specifically been set as no value (ie. null). Undefined means that a variable has been declared but its value has not yet been assigned"
+      },
+      {
+        "question": "What is this [[[ ]]]?",
+        "answer": "Refers to a three-dimentional array"
+      },
+      {
+        "question":
+          "What are the differences between cookie, local storage, and session storage?",
+        "answer": "All three refer to storing client information on the browser to be retrieved and used at a later time. Local Storage - stores user information without any expiration date. Ie. it’s there until the user goes into their local storage and chooses to delete it. Information is stored as a json object in the form of key/value pairs. This data can only be read on the client side and not accessed by the server. Session Storage - stores data for one sessions (ie. typically data is lost when the browser tab is closed). Session storage is accessible from the server. Cookies act almost like a access card to the session information - and allow us to extend or shorten session lifespans (this means that information can persist after the browser is closed - or we can set sessions to end even within the browser lifespan)"
+      },
+      {
+        "question": "What is the difference between setInterval and setTimeout",
+        "answer": "Settimeout allows us to run a function - or set block of code -  a single time after the interval of time declared by the timeout. To clear, clearTimeout(). settimeout(function ,0) will place the function execution on the waiting section of the stack, and will be run once the stack is empty. Set interval allows us to a run function - or set block of code - repeatedly starting after the interval of time, then repeating continuously at that interval. To clear, clearInterval()"
+      },
+      {
+        "question": "What is the 'this' keyword? ",
+        "answer": "This keyword refers to an object - and can be used to access properties of an object. Depending on how it is used it can refer to different objects. Ie. when used in an object method, it refers to the object. Alone, it refers to the global object. In a web browser - the global object is the window. In something like node, there is global as the global object"
+      }
+  ]);
+
+  console.log('questions seeded');
+
+  process.exit();
+
+});
--- server/server.js
+++ server/server.js
@@ -3,8 +3,9 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,7 +15,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
