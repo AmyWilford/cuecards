476f1b3461a99eeca015984a5dc535581a50c807
Amy <wilford.amy@gmail.com>
2023-01-10 15:05:38 -0500

    test on button nav

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,28 +1,30 @@
 // Import required components and dependencies
 import React from "react";
-import lightbulb from "../assets/lightbulb.png";
-import styled from "styled-components";
-
-
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components"
 
 export default function Header() {
 
+  const navigate = useNavigate();
 
-  
+  const routeChangeStudy = () => {
+    let path = `/study`;
+    navigate(path);
+  };
   // Function to show different nav elements based on login status
-
   return (
     <div className="header fixed-top pt-3 bg-white d-flex justify-content-end">
       <nav className="navbar">
           <div>
           <ul className="navbar-nav nav-options">
             <li className="nav-item">
-            <a
-              href="/study"
-              className="nav-link"
-            >
-              start studying
-            </a>
+            <button
+            className="btn btn-link nav-link m-1"
+            type="submit"
+            onClick={routeChangeStudy}
+          >
+          start studying
+          </button>
             </li>
           </ul>
           </div>
