916bf697d98fa45bee9460f0c1db909ef3801a3b
AmyWilford <wilford.amy@gmail.com>
2023-02-17 15:10:26 -0500

    added home page to tourte and styled start studying section

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Home from "./components/Home";
+import Home from "./components/Home";
 import Question from "./components/Question";
 import AddQuestion from "./components/AddQuestion";
 import DBQuestion from "./components/AllQuestions";
@@ -15,8 +15,8 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
-        <Route path="/" element={<DBQuestion />} />
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<DBQuestion />} />*/ }
         <Route path="/study" element={<Question />} />
         <Route path="/api/study" element={<AddQuestion />} />
         <Route path="/api/study/:id" element={<Update />} />
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,14 +1,14 @@
 import React from "react";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 // import styled from "styled-components";
 import lightbulb from "../assets/lightbulb.png";
 
 export default function Home() {
-  // let navigate = useNavigate();
-  // const routeChangeStudy = () => {
-  //   let path = `/study`;
-  //   navigate(path);
-  // };
+  let navigate = useNavigate();
+  const routeChangeStudy = () => {
+    let path = `/study`;
+    navigate(path);
+  };
 
   return (
     <div className="row d-flex flex-column align-content-center mt-5">
@@ -16,9 +16,9 @@ export default function Home() {
         <img src={lightbulb} alt="lighbulb graphic"></img>
       </div>
 
-      {/* <button className="btn custom-button m-2" onClick={routeChangeStudy}>
-        start studying
-      </button> */}
+      <span className="home-study-button" onClick={routeChangeStudy}>
+        start studying ➡
+      </span>
      
     </div>
   );
