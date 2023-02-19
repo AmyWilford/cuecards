befbc0b31359f95351773186db4708e9bf9e1f9c
AmyWilford <wilford.amy@gmail.com>
2023-02-19 16:08:35 -0500

    adjusted footer styles

--- client/src/components/AddQuestion.js
+++ client/src/components/AddQuestion.js
@@ -61,7 +61,7 @@ export default function AddQuestion() {
       return;
     });
     setNewQuestion({ question: " ", answer: "" });
-    navigate("/");
+    navigate("/allcards");
   };
   return (
     <div className="container">
